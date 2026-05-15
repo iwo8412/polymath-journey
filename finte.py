757857dc5ed5d60e1d8cf1e9e0d755d542597c2b
iwooo <122059934+iwo8412@users.noreply.github.com>
2026-05-15 11:36:56 +0200

    Create finte.py

---
+++ finte.py
@@ -0,0 +1,4 @@
+wide_pushups = input('Скільки широких віджимань ти зробив сьогодні? ')
+print(f'Круто! Записано широких віджимань: {wide_pushups}')
+total_pushups = int(wide_pushups) + 10
+print(f'Твій загальний прогрес: {total_pushups}')
